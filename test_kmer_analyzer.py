c2cad73802e039caa6d185e6b022bed00b29f5f2
Emma Brookins <emmabrookins@uri.edu>
2026-05-04 15:13:03 -0400

    Added tests for write_results_to_file function and ensured all ran smoothly with pytest.

--- test_kmer_analyzer.py
+++ test_kmer_analyzer.py
@@ -92,8 +92,64 @@ def test_count_kmers_with_context_different():
   obs2=result["TG"]["C"]
   exp2=1
   assert obs2 == exp2
+
+from kmer_analyzer import write_results_to_file
+import os 
+
+#First test for one kmer and one next character 
+def test_write_results_to_file_single():
+  kmer_data={"AT": {"G": 2}}
+  output_file="test_output_single.txt"
+  write_results_to_file(kmer_data,output_file)
+  with open (output_file, "r") as f:
+    contents = f.read().strip()
+  obs=contents
+  exp="AT (2): G:2"
+  assert obs == exp
+  os.remove(output_file)
   
-  
+#Test for one kmer and multiple next characters 
+def test_write_results_to_file_one_kmer_multiple_chars():
+    kmer_data={"AT": {"C": 1, "G": 2}}
+    output_file="test_output_single_kmer_multiple_chars.txt"
+    write_results_to_file(kmer_data, output_file)
+    with open (output_file, "r") as f:
+        contents = f.read().strip()
+    obs=contents
+    exp = "AT (3): C:1, G:2"
+    assert obs == exp
+    os.remove(output_file)
+
+#Test for more than one kmer 
+def test_write_results_to_file_multiple():
+    kmer_data={
+        "TG": {"C": 1},
+        "AT": {"G": 2}
+    }
+    output_file="test_output_multiple.txt"
+    write_results_to_file(kmer_data, output_file)
+    with open(output_file, "r") as f:
+        contents = f.read().strip().split("\n")
+    obs=contents
+    exp = [
+        "AT (2): G:2",
+        "TG (1): C:1"
+    ]
+    assert obs == exp
+    os.remove(output_file)
+
+#Test to make sure the total is correct 
+def test_write_results_to_file_total_correct():
+    kmer_data={"AA": {"A": 3, "T": 1}}
+    output_file="test_output_total.txt"
+    write_results_to_file(kmer_data, output_file)
+    with open(output_file, "r") as f:
+        contents=f.read().strip()
+    obs=contents
+    exp="AA (4): A:3, T:1"
+    assert obs == exp
+    os.remove(output_file)
+
 
 def test_placeholder():
     assert True
